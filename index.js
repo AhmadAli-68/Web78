69fd38c08c2729fcbb2b4acd37b16c8af285a0af
Ahmad Ali <ahmad.ali619x@gmail.com>
2024-09-30 15:58:27 +0500

    Updated Repo
    Updated

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 alert("Hi Mohsin");
+alert("Hi Ahmad");
+
+let a = 45;
+console.log(a);
+console.log(typeof (a));
+
+let b = "56"
+console.log(b);
+
+let c = true;
+console.log(c);
+console.log(typeof (c));
+
+console.log(4 + "4");
+console.log(typeof (4 + "4"));
+
+console.log("4" - 4);
+console.log(typeof ("4" - 4));
+
+name = prompt("Enter your name:");
+
+document.write(name);
+
+console.log(5 ** 3);
+
+//Brackets orders
+
+
+
+
